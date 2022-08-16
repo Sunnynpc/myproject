f23876cd1b6f003219cee791f28d99f11acfcd1e
Sunnynpc <111328879+Sunnynpc@users.noreply.github.com>
2022-08-16 17:09:39 +0800

    Add files via upload

---
+++ pythonProject03/resnet_34_layer.py
@@ -0,0 +1,78 @@
+import torch
+from torch import nn
+
+class Res_Block(nn.Module):
+    def __init__(self, c):
+        super(Res_Block, self).__init__()
+        self.layer = nn.Sequential(
+            nn.Conv2d(c, c, 3,1, padding=1, bias=False),
+            nn.BatchNorm2d(c),
+            nn.ReLU(),
+            nn.Conv2d(c, c, 3, 1, padding=1, bias=False),
+            nn.BatchNorm2d(c),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        return self.layer(x) + x
+
+class Pool(nn.Module):
+    def __init__(self,c_in,c_out):
+        super(Pool, self).__init__()
+        self.layer = nn.Sequential(
+            nn.Conv2d(c_in, c_out, 3, 2, padding=1,bias=False),
+            nn.BatchNorm2d(c_out),
+            nn.ReLU(),
+            nn.Conv2d(c_out, c_out, 3, 1, padding=1,bias=False),
+            nn.BatchNorm2d(c_out),
+            nn.ReLU()
+        )
+    def forward(self,x):
+        return self.layer(x)
+
+class Res_Net34(nn.Module):
+    def __init__(self):
+        super(Res_Net34, self).__init__()
+        self.layer = nn.Sequential(
+            nn.Conv2d(3, 64, 7, 2, padding=3, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(3, 2),
+
+            Res_Block(64),
+            Res_Block(64),
+            Res_Block(64),
+
+            Pool(64, 128),
+            Res_Block(128),
+            Res_Block(128),
+            Res_Block(128),
+
+            Pool(128, 256),
+            Res_Block(256),
+            Res_Block(256),
+            Res_Block(256),
+            Res_Block(256),
+            Res_Block(256),
+
+            Pool(256, 512),
+            Res_Block(512),
+            Res_Block(512),
+
+            nn.AdaptiveAvgPool2d((1, 1))
+        )
+
+        self.out_layer = nn.Sequential(
+            nn.Linear(512, 1000, bias=True),
+        )
+
+    def forward(self, x):
+        conv_out = self.layer(x)
+        conv_out = conv_out.reshape(-1, 512)
+        out = self.out_layer(conv_out)
+        return out
+
+
+if __name__ == '__main__':
+    net = Res_Net34()
+    print(net)
---
+++ pythonProject03/resnet_50_layer.py
@@ -0,0 +1,87 @@
+import torch
+from torch import nn
+
+class Res_Block(nn.Module):
+    def __init__(self, c_in, c_out):
+        super(Res_Block, self).__init__()
+        self.layer = nn.Sequential(
+            nn.Conv2d(c_in, c_out, 1,1, padding=1, bias=False),
+            nn.BatchNorm2d(c_out),
+            nn.ReLU(),
+            nn.Conv2d(c_out, c_out, 3, 1, padding=1, bias=False),
+            nn.BatchNorm2d(c_out),
+            nn.ReLU(),
+            nn.Conv2d(c_out, c_in, 1, 1, padding=1, bias=False),
+            nn.BatchNorm2d(c_in),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        return self.layer(x) + x
+
+
+
+class Pool(nn.Module):
+    def __init__(self,c_1,c_2, c_3, s):
+        super(Pool, self).__init__()
+        self.layer = nn.Sequential(
+            nn.Conv2d(c_1, c_2, 1, stride=s, padding=1,bias=False),
+            nn.BatchNorm2d(c_2),
+            nn.ReLU(),
+            nn.Conv2d(c_2, c_2, 3, 1, padding=1,bias=False),
+            nn.BatchNorm2d(c_2),
+            nn.ReLU(),
+            nn.Conv2d(c_2, c_3, 1, 1, padding=1, bias=False),
+            nn.BatchNorm2d(c_3),
+            nn.ReLU()
+        )
+    def forward(self,x):
+        return self.layer(x)
+
+class Res_Net50(nn.Module):
+    def __init__(self):
+        super(Res_Net50, self).__init__()
+        self.layer = nn.Sequential(
+            nn.Conv2d(3, 64, 7, 2, padding=3, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(3, 2),
+
+            Pool(64, 64, 256, 1),
+            Res_Block(256, 64),
+            Res_Block(256, 64),
+
+            Pool(256, 128, 512, 2),
+            Res_Block(512, 128),
+            Res_Block(512, 128),
+            Res_Block(512, 128),
+
+            Pool(512, 256, 1024, 2),
+            Res_Block(1024, 256),
+            Res_Block(1024, 256),
+            Res_Block(1024, 256),
+            Res_Block(1024, 256),
+            Res_Block(1024, 256),
+
+            Pool(1024, 512, 2048, 2),
+            Res_Block(2048, 512),
+            Res_Block(2048, 512),
+
+
+            nn.AdaptiveAvgPool2d((1, 1))
+        )
+
+        self.out_layer = nn.Sequential(
+            nn.Linear(2048, 1000, bias=True),
+        )
+
+    def forward(self, x):
+        conv_out = self.layer(x)
+        conv_out = conv_out.reshape(-1, 2048)
+        out = self.out_layer(conv_out)
+        return out
+
+if __name__ == '__main__':
+    net = Res_Net50()
+
+    print(net)
